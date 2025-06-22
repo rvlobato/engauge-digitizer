a450e791d639e464f3da64a0d8d2bf5b624f2d1b
Andreas K. Hüttel <dilfridge@gentoo.org>
2025-06-22 18:29:17 +0200

    Set version to 12.9 to indicate beta status or similar
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- src/util/Version.cpp
+++ src/util/Version.cpp
@@ -10,7 +10,7 @@
 // 1) changes to Document class
 // 2) at least one version-specific test case in the test subdirectory
 // 3) at least one period in the version number (e.g. 13.0 versus 13)
-const char *VERSION_NUMBER = "13.0";
+const char *VERSION_NUMBER = "12.9";
 
 QString engaugeWindowTitle()
 {
